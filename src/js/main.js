cc7290eda6a3a1e56bf8f1ef2895e166b8abb9b4
Felipe Zuntini <felipezuntini@hotmail.com>
2023-03-20 15:58:14 -0300

    fix: 🐛 header path

--- src/js/main.js
+++ src/js/main.js
@@ -3,23 +3,24 @@ import htm from 'https://unpkg.com/htm?module';
 const html = htm.bind(h);
 
 render(createElement(() => {
+	const sufix = document.location.hostname === 'localhost' ? './' : './kaltura-player-and-csp-headers/';
 	return html`
 		<bx-header aria-label="IBM Platform Name">
-			<bx-header-name href="/" prefix="IBM">
+			<bx-header-name href="${sufix}" prefix="IBM">
 				Player Test
 			</bx-header-name>
 			<bx-header-nav menu-bar-label="IBM [Platform]">
-				<bx-header-nav-item href="/">
+				<bx-header-nav-item href="${sufix}">
 					Kaltura NO CSP
 				</bx-header-nav-item>
 			</bx-header-nav>
 			<bx-header-nav menu-bar-label="IBM [Platform]">
-				<bx-header-nav-item href="/kaltura-with-csp.html">
+				<bx-header-nav-item href="${sufix}kaltura-with-csp.html">
 					Kaltura With CSP
 				</bx-header-nav-item>
 			</bx-header-nav>
 			<bx-header-nav menu-bar-label="IBM [Platform]">
-				<bx-header-nav-item href="/youtube-with-csp.html">
+				<bx-header-nav-item href="${sufix}youtube-with-csp.html">
 					YouTube With CSP
 				</bx-header-nav-item>
 			</bx-header-nav>
